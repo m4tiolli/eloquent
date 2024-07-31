8b36f8139dd433747ed8a07a60a4714d22c93c46
Gabriel Matiolli <gabrielmatiollif@gmail.com>
2024-07-31 16:04:37 -0300

    Router Header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
 import LoginButton from "./Login.Button";
 import RegisterButton from "./Register.Button";
+import { useRouter } from "next/navigation";
 
 export default function Header() {
+
+  const router = useRouter();
+
   return (
     <div className="w-full fixed z-10 top-0 px-4 h-[10dvh] backdrop-blur flex items-center justify-between">
       <div className="w-1/3">
@@ -16,7 +20,7 @@ export default function Header() {
       </div>
       <div className="xl:w-1/3 flex items-center justify-end gap-4">
         <LoginButton />
-        <RegisterButton />
+        <RegisterButton router={router} />
       </div>
     </div>
   );
--- components/Header/Register.Button.tsx
+++ components/Header/Register.Button.tsx
@@ -1,3 +1,9 @@
-export default function RegisterButton() {
-  return <button className="intense-button">Sign up</button>;
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+
+interface RouterProp {
+  router: AppRouterInstance
+}
+
+export default function RegisterButton({router}: RouterProp) {
+  return <button className="intense-button" onClick={() => router.push("/register")}>Sign up</button>;
 }
